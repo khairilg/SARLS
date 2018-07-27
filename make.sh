6a62c83fc988d9196bd66166f8607c78479dc894
Khairil Gunawan <khairilgunawan@live.com>
2018-07-28 01:04:52 +0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -10,13 +10,14 @@ read -p "Enter notification if the service is stopped (example: not running)" pr
 if [ ! -d "/etc/sarls" ]; then
 mkdir /etc/sarls
 fi
+rm -y /etc/sarls/$procname
 echo '#! /bin/sh' >> /etc/sarls/$procname
-echo 'if /etc/init.d/$procname status | grep -q "$procnote";' >> /etc/sarls/$procname
+echo 'if /etc/init.d/'$procname' status | grep -q "'$procnote'";' >> /etc/sarls/$procname
 echo 'then' >> /etc/sarls/$procname
-echo 'echo "service $procname is not running, restarting.."' >> /etc/sarls/$procname
-echo '/etc/init.d/$procname restart' >> /etc/sarls/$procname
+echo 'echo "service '$procname' is not running, restarting.."' >> /etc/sarls/$procname
+echo '/etc/init.d/'$procname' restart' >> /etc/sarls/$procname
 echo 'else' >> /etc/sarls/$procname
-echo 'echo "service $procname is running."' >> /etc/sarls/$procname
+echo 'echo "service '$procname' is running."' >> /etc/sarls/$procname
 echo 'fi' >> /etc/sarls/$procname
 
 # Create crontab for running auto restart file, run every 5 minutes
