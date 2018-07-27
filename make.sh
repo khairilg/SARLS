eb9c93ae88feda51ffac375f4ae469a590b44eba
Khairil Gunawan <khairilgunawan@live.com>
2018-07-28 01:40:11 +0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -21,9 +21,6 @@ echo 'echo "service '$procname' is running."' >> /etc/sarls/$procname
 echo 'fi' >> /etc/sarls/$procname
 
 # Create crontab for running auto restart file, run every 5 minutes
-if crontab -l || grep -q "'$procname'";
-then
-crontab -l | { cat; echo "*/5 * * * * sh /etc/sarls/$procname > /dev/null 2>&1"; } | crontab -
-fi
+( crontab -l | { grep -v -F "sh /etc/sarls/$procname" ; echo "*/5 * * * * sh /etc/sarls/$procname > /dev/null 2>&1"; } ) | crontab -
 echo "Cron Script Simple Auto Restart Linux Service Is Finish"
 echo "Cron file placed in /etc/sarls/$procname"
